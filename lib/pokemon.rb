1a095b991d4c0a1acad29cfffa012cac1eb1a40d
louis baptiste <louisvis@ymail.com>
2018-10-06 06:25:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
+require "pry"
 class Pokemon
+attr_accessor :name, :id, :type, :db
+
+  def initialize(id:, name:,type:, db:, hp:60)
+      @id   = id
+      @name = name
+      @type = type
+      @db   = db
+      @hp   = hp
+
+  end
+
+  def self.save(name, type, db,)
+   db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?)", name, type, 60)
+ end
+
+ def self.find (id, db)
+   found_pokemon = db.execute ("SELECT * FROM pokemon WHERE id =#{id}")
+   flatten_pokemon = found_pokemon.flatten
+   pokemon_object = self.new(id:flatten_pokemon[0], name:flatten_pokemon[1],type: flatten_pokemon[2], db:db)
+   pokemon_object
+ end
+
 end
