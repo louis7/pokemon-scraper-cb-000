f2a92b607422c39f2b09db79ced2f612898f12e5
louis baptiste <louisvis@ymail.com>
2018-10-06 06:28:22 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ attr_accessor :name, :id, :type, :db
   end
 
   def self.save(name, type, db)
-   db.execute("INSERT INTO pokemon (name, type,) VALUES (?, ?)", name, type,)
+   db.execute("INSERT INTO pokemon (name, type,) VALUES (?, ?)", name, type)
  end
 
  def self.find (id, db)
